9cbb2349476bd70c2cb5e2c005d089bf6c682813
Abishek <abhi8569@gmail.com>
2019-07-18 19:03:50 +0200

    -Added Fitness Variable

--- PSO_RDD/src/PSO_RDD.scala
+++ PSO_RDD/src/PSO_RDD.scala
@@ -10,11 +10,11 @@ import org.apache.log4j.Level
 object PSO_RDD {
   
   var dimension = 20
-  var no_of_particles = 1500
-  var no_of_iteration_External = 10
-  var no_of_iteration_Internal = 500
-  var gbest_position=Array.fill(dimension)(math.random)
-    
+  var no_of_particles = 500
+  var no_of_iteration_External = 1000
+  var no_of_iteration_Internal = 1
+  var g_best=Array.fill(dimension)(math.random)
+  var gbest_fitness = 0.0   
   def main(args:Array[String]): Unit ={
     
     Logger.getLogger("org").setLevel(Level.OFF)
@@ -26,47 +26,53 @@ object PSO_RDD {
     
     var swarm = ArrayBuffer[Particle]()
     for(i <- 0 to no_of_particles-1){
-      swarm += new Particle(dimension,i) with Serializable
-      
+      swarm += new Particle(dimension,i) with Serializable 
     }
     var swarm_rdd = sc.parallelize(swarm,8)
 
-    swarm_rdd.foreach(f => global_best_position(f.p_position))
     swarm_rdd.foreach(f => init_pbest(f))
+    swarm_rdd.foreach(f => f.compute_pbest_fitness())
+    swarm_rdd.foreach(f => f.compute_pfitness())
+    swarm_rdd.foreach(f => global_best_position(f))
+    
     var temp_rdd: RDD[Particle]= sc.emptyRDD[Particle]
     temp_rdd=swarm_rdd
     var updated_velocity_swarm: RDD[Particle]= sc.emptyRDD[Particle]
     
-    var bCast = sc.broadcast(gbest_position)
+    var bCast = sc.broadcast(g_best)
     
-    println(obj_func(gbest_position))
+    println(obj_func(g_best))
     for(iteration <- 0 to no_of_iteration_External){
       updated_velocity_swarm = temp_rdd.map{x => update_particle(x)}
       temp_rdd = updated_velocity_swarm
-      temp_rdd.collect().foreach(f => global_best_position(f.p_best))
-      println(obj_func(gbest_position))
+      temp_rdd.collect().foreach(f => global_best_position(f))
+      println(obj_func(g_best))
       bCast.unpersist(blocking=true)
-      bCast = sc.broadcast(gbest_position)
+      bCast = sc.broadcast(g_best)
     }
     
     def update_particle(p:Particle):Particle={
-      gbest_position = bCast.value
+      g_best = bCast.value
      for(iter <- 0 to no_of_iteration_Internal){
       for(i <- 0 to dimension-1){
         var toward_pbest= math.random*(p.p_best(i)-p.p_position(i))
-        var toward_gbest =  2*math.random*(gbest_position(i)-p.p_position(i))
+        var toward_gbest =  2*math.random*(g_best(i)-p.p_position(i))
         p.p_velocity(i) = 0.5*p.p_velocity(i) + toward_pbest + toward_gbest
       }
       for(i <- 0 to dimension-1){
         p.p_position(i) = p.p_position(i) + p.p_velocity(i)
       }
       
-      if(obj_func(p.p_position) < obj_func(p.p_best)){
+      p.compute_pfitness()
+      
+      if(p.p_fitness < p.pbest_fitness){
         p.p_best = p.p_position
       }
       
-      if(obj_func(p.p_best) < obj_func(gbest_position)){
-        gbest_position=p.p_position
+      p.compute_pbest_fitness()
+      
+      if(p.pbest_fitness < obj_func(g_best)){
+        g_best=p.p_position
       }
       //println("Iteration Number : "+iter+" || Particle Number :  "+p.p_id+" || Pbest : "+obj_func(p.p_best)+" || Gbest : "+obj_func(gbest_position))
       }
@@ -89,9 +95,9 @@ object PSO_RDD {
     }
   
   
-  def global_best_position(pos:Array[Double])={
-    if(obj_func(pos) < obj_func(gbest_position)){
-      gbest_position=pos
+  def global_best_position(p:Particle)={
+    if(p.pbest_fitness < obj_func(g_best)){
+      g_best=p.p_best
     }
   }  
   
--- PSO_RDD/src/Particle.scala
+++ PSO_RDD/src/Particle.scala
@@ -6,4 +6,24 @@ class Particle(dimension:Int,iter:Int) extends Serializable  {
   var p_velocity = (Seq.fill(dimension)(math.random)).toArray
   var p_best = (Seq.fill(dimension)(math.random)).toArray
   
+  var p_fitness = 0.0
+  var pbest_fitness = 0.0
+  
+  def compute_pfitness() ={
+    p_fitness=obj_func(p_position)
+  }
+  
+  def compute_pbest_fitness() ={
+    pbest_fitness=obj_func(p_best)
+  }
+  
+  def obj_func(x:Array[Double]):Double = {
+      var temp:Double  =0
+      for(dim <- 0 to x.length-1 )
+      {
+        temp =temp + (math.pow(x(dim),2))
+      }
+      return temp
+    }
+  
 }
